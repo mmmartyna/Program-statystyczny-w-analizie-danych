5655fefcc9675153a408562d49f6da4282024e2f
Martyna Szawłowska <mszawlowska@INF.UG.EDU.PL>
2024-10-10 09:56:41 +0200

    dodanie funkcji2 i liniowej funkcji dyskryminacyjnej

--- lab02.R
+++ lab02.R
@@ -53,3 +53,37 @@ predykcja = function(x) {
 example_point = c(-1, -0.2)  # Change this to test different points
 predykcja(example_point)
 
+# funkcja predykcji przy założeniu różnych prawdopodobieństw apriori dla klas
+predykcja2 = function(x){
+  
+  dd = t(x-0.5*(mu1+mu0))%*%solve(S)%*%(mu1-mu0)+log((k1/(k1+k0))/(k0/(k1+k0)))
+  if (dd>=0){
+    return(1)
+  }
+  else{
+    return(0)
+    }
+}
+
+pred = apply(daneL[,1:2], 1, predykcja)
+pred
+acc = length(klasa[klasa == pred]) / length(klasa)
+acc
+
+pred2 = apply(daneL[,1:2], 1, predykcja2)
+pred2
+acc2 = length(klasa[klasa == pred2]) / length(klasa)
+acc2
+
+poprawka = log((k1/(k1+k0))/(k0/(k1+k0)))
+
+daneL$pred=pred
+head(daneL)
+
+w = solve(S)%*%(mu1 - mu0)
+w
+s = -0.5*(t(mu1+mu0)%*%w)
+s
+plot(daneL$x1, daneL$x2, type = "n", xlab = "x1", ylab = "x2", main = "Liniowa funkcja dyskryminacyjna")
+points(daneL$x1[daneL$klasa == 1], daneL$x2[daneL$klasa == 1], col = "red", cex = 0.5)
+points(daneL$x1[daneL$klasa == 0], daneL$x2[daneL$klasa == 0], col = "blue", cex = 0.5)
